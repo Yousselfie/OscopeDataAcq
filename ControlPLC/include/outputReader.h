c7832da8f394da27b68886a6739ace4af1b8aee8
Youssef Bahloul <ybahloul77@gmail.com>
2025-06-30 12:10:42 -0400

    Cleaned up code with functions; Added output reading and checking if it matches expected output

--- ControlPLC/include/outputReader.h
+++ ControlPLC/include/outputReader.h
@@ -8,8 +8,7 @@
 //Each input must have an output, so there can be a maximum of 27 input and 27 output pins
 //Allocate pins 0-26 for input and 27-53 for output
 //*PINS THAT CAN'T BE USED: 
-const int MAX_INPUT_PIN = 26;
-const int MAX_OUTPUT_PIN = 53;
+
 
 void setupOutputReader();
 void loopOutputReader();
